59dd7c28011d08cd36affba645d016b34338340d
HeartCNC <32353232+HeartCNC@users.noreply.github.com>
2018-05-15 21:54:05 +0800

    Create datebase.php

---
+++ datebase.php
@@ -0,0 +1,46 @@
+<?php
+	/*增加，删除，修改，查询*/
+	class User{
+		var $DB_HOST = "localhost";
+		var $DB_USER = "root";
+		var $DB_PASSWORD = "root";
+		var $DB_NAME = "db";
+		
+		//连接数据库
+		function connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME){
+			$this->DB_HOST = $DB_HOST;
+			$this->DB_USER = $DB_USER;
+			$this->DB_PASSWORD = $DB_PASSWORD;
+			$this->DB_NAME = $DB_NAME;
+			if( $conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME) ){
+				return $conn;
+			}
+			return false;
+		}
+		//关闭数据库
+		function close($conn){
+			mysqli_close($conn);
+		}
+		
+		//返回数据
+		function queryDate($tableName,$field,$where="",$start=0,$length=0){
+			if( $this->connect() ){
+				$end = $start + $length - 1;
+				if( $length > 0 )
+					$sql = "select {$field} where {$where} from {$tableName} limit {$start},{$end}";
+				else
+					$sql = "select {$field} where {$where} from {$tableName}";
+				
+				$this->close();
+			}
+		}
+		
+		//删除记录
+		function deleteDate(){
+			
+		}
+		//增加记录
+		
+		//修改记录
+	}
+?>
